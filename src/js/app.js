977d74999c9273dbf3fd99f9e84c4a54631dd1dd
Girda <artem.girda@gmail.com>
2018-05-27 18:39:14 +0300

    up

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,15 @@
 import './components/tabs.js';
 import './components/popup.js';
+import './components/accordion.js';
 // import './components/slider-popup.js';
 import 'slick-carousel';
 
 $(document).ready( () => {
   $('.js-slider').slick({
-  	slidesToShow: 3
+  	slidesToShow: 3,
+  	// centerMode: true,
+    prevArrow: '<button class="slider__btn slider__btn--prev" type="button"><img src="img/arrow-left.svg"></button>',
+    nextArrow: '<button class="slider__btn slider__btn--next" type="button"><img src="img/arrow.svg"></button>'
   });
 
 });
